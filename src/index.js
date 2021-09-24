94045658a85e3e8bbad58d300e0bb7dd74da50bc
HannahConnolly <connollyhannah93@gmail.com>
2021-09-24 16:31:16 -0700

    initial logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 console.log('test');
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const PORT = 5555;
+const { exec } = require('child_process');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  console.log('endpoint hit!');
+  exec('pihole disable 30s', (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    return;
+  });
+  console.log(req.query);
+  res.send('recieved..');
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 🌎 App listening on port ${PORT} 🌎 🌎`);
+});
